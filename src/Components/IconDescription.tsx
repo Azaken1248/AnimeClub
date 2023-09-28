86312e1b0ac36d4543730ec47b7720ba3bd55e60
Azaken1248 <suvkrm@gmail.com>
2023-09-29 01:47:34 +0530

    Making Basic Homepage template

---
+++ src/Components/IconDescription.tsx
@@ -0,0 +1,20 @@
+// import logoblack from '../assets/logo-black.png';
+import "../styles/IconDescription.css";
+
+const IconDescription = () => {
+  return (
+    <section className="icon-description">
+      <div className="home-heading">
+        <h1 style={{ fontFamily: "cursive" }}>Anime Realm</h1>
+        <h2 style={{ fontFamily: "cursive" }}>
+          VITC's First Ever Official Anime Club
+        </h2>
+      </div>
+      {/* 
+      <img className="home-title-image" src={logoblack} alt="Logo"></img>
+      */}
+    </section>
+  );
+};
+
+export default IconDescription;
