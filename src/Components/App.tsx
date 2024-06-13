fe7ea0c50b1ba83da609b6b152e70056d30595bf
Azaken1248 <suvkrm@gmail.com>
2024-06-13 18:16:18 +0530

    Adding Entry Page And CSS Hotfixes

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -1,51 +1,7 @@
-import { SetStateAction, useState } from "react";
-import NavigationBar from "./NavigationBar";
-import "../Styles/App.css";
 import HomePage from "./homePage";
 
 function App() {
-  const [current_page, setCurrentPage] = useState("home");
-  var cp = current_page;
-  var cpp = cp;
-  var cp = cpp;
-  if (current_page == "home") {
-    return (
-      <>
-        <NavigationBar setCurrentPage={setCurrentPage} />
-        <div className="nav-fixer"></div>
-        <HomePage setCurrentPage={setCurrentPage}></HomePage>
-      </>
-    );
-  } else if (current_page == "events") {
-    return (
-      <>
-        <NavigationBar setCurrentPage={setCurrentPage} />
-        <div className="nav-fixer"></div>
-      </>
-    );
-  } else if (current_page == "members") {
-    return (
-      <>
-        <NavigationBar setCurrentPage={setCurrentPage} />
-        <div className="nav-fixer"></div>
-      </>
-    );
-  } else if (current_page == "contact us") {
-    return (
-      <>
-        <NavigationBar setCurrentPage={setCurrentPage} />
-        <div className="nav-fixer"></div>
-      </>
-    );
-  } else {
-    return (
-      <>
-        <NavigationBar setCurrentPage={setCurrentPage}></NavigationBar>
-        <div className="nav-fixer"></div>
-        <h1>{current_page}</h1>
-      </>
-    );
-  }
+  return <HomePage />;
 }
 
 export default App;
--- src/Components/IconDescription.tsx
+++ src/Components/IconDescription.tsx
@@ -1,18 +1,50 @@
-// import logoblack from '../assets/logo-black.png';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAngleDoubleDown } from "@fortawesome/free-solid-svg-icons";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import "../styles/IconDescription.css";
 
 const IconDescription = () => {
   return (
     <section className="icon-description">
-      <div className="home-heading">
-        <h1 style={{ fontFamily: "cursive" }}>Anime Realm</h1>
-        <h2 style={{ fontFamily: "cursive" }}>
-          VITC's First Ever Official Anime Club
-        </h2>
+      <div className="content-box">
+        <div className="nav">
+          <div className="menu">
+            Menu
+            <div className="hamburger">
+              <div className="line"></div>
+              <div className="line-break">
+                <div className="line-half"></div>
+                <div className="line-half-color"></div>
+              </div>
+            </div>
+          </div>
+          <div className="profile">
+            <AccountCircleIcon
+              style={{
+                color: "#ffffff",
+                background: "#000000",
+                borderRadius: "100%",
+                width: "60px",
+                height: "60px",
+                cursor: "pointer",
+              }}
+            />
+          </div>
+        </div>
+        <h1 className="title" data-text="ANIME REALM">
+          ANIME REALM
+        </h1>
+        <div className="scrollbar">
+          <FontAwesomeIcon
+            icon={faAngleDoubleDown}
+            className="fa-angle-double-down"
+          />
+          <FontAwesomeIcon
+            icon={faAngleDoubleDown}
+            className="fa-angle-double-down"
+          />
+        </div>
       </div>
-      {/* 
-      <img className="home-title-image" src={logoblack} alt="Logo"></img>
-      */}
     </section>
   );
 };
